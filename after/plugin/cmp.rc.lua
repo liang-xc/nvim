86765d851cc2968c1012c953a6b991e18e39b684
LIANG X <liangx0111@outlook.com>
2023-01-02 16:50:25 -0500

    more keymap

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -1,9 +1,10 @@
+local luasnip = require('luasnip')
 local cmp = require('cmp')
 
 cmp.setup({
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
@@ -11,11 +12,33 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'buffer' },
+    { name = 'luasnip' },
   }),
   sorting = {
     comparators = {
--- lua/maps.lua
+++ lua/maps.lua
@@ -1,4 +1,5 @@
 vim.keymap.set('n', '<leader>s', ':w<Return>')
+vim.keymap.set('n', '<leader>h', ':ClangdSwitchSourceHeader<Return>')
 
 vim.keymap.set('n', 'J', "mzJ`z")
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -6,5 +7,18 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'nzzzv')
 
+-- New tab
+vim.keymap.set('n', 'te', ':tabedit')
+-- Split window
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+-- Move window
+vim.keymap.set('n', '<Space>', '<C-w>w')
+vim.keymap.set('', 'sh', '<C-w>h')
+vim.keymap.set('', 'sk', '<C-w>k')
+vim.keymap.set('', 'sj', '<C-w>j')
+vim.keymap.set('', 'sl', '<C-w>l')
+
+-- Move selected up and down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
