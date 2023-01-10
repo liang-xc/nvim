7551852cb2f62f50ffa587d49522969d55a97180
LIANG Xiaocheng <97717068+liang-xc@users.noreply.github.com>
2023-01-10 15:20:18 -0500

    add null-ls

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,29 +2,46 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+
+  -- colorscheme and UI
   use 'EdenEast/nightfox.nvim' -- theme nordfox
   use 'nvim-lualine/lualine.nvim' -- statusline
+  use "lewis6991/gitsigns.nvim"
+
+  -- helpers
+  use 'windwp/nvim-autopairs'
+
+  -- LSP
   use 'neovim/nvim-lspconfig' -- lsp
   use 'onsails/lspkind-nvim' -- vscode-like pictograms
   use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in lsp
   use 'hrsh7th/cmp-cmdline' -- nvim-cmp source for command
+  use 'hrsh7th/cmp-nvim-lua' -- nvim lua runtime API completion
+  use 'hrsh7th/cmp-path' -- nvim-cmp source for path
   use 'hrsh7th/nvim-cmp' -- completion
   use 'L3MON4D3/LuaSnip' -- snippet
+
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  }
+
+  use "p00f/clangd_extensions.nvim" -- more from clangd
+
+  -- diagnostics and formatting
+  use "jose-elias-alvarez/null-ls.nvim"
+
+  -- treesitter
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
-  use 'windwp/nvim-autopairs'
+
+  -- telescope
   use {
     'nvim-telescope/telescope.nvim',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use 'nvim-telescope/telescope-file-browser.nvim'
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  }
-  use "lewis6991/gitsigns.nvim"
-  use "p00f/clangd_extensions.nvim"
 end)
