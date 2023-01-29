f03d69ba9abdb12615b5c0457535bbee260e0997
LIANG Xiaocheng <97717068+liang-xc@users.noreply.github.com>
2023-01-29 12:32:05 -0500

    rearrange base config

--- lua/base.lua
+++ lua/base.lua
@@ -4,25 +4,25 @@ vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
-vim.wo.number = true
-vim.wo.relativenumber = true
-
-vim.opt.title = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.breakindent = true
-vim.opt.expandtab = true
-vim.opt.scrolloff = 5
-vim.opt.inccommand = 'split'
 vim.opt.backup = false
 vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
-
-vim.opt.incsearch = true
+vim.opt.breakindent = true
+vim.opt.expandtab = true
 vim.opt.ignorecase = true
+vim.opt.inccommand = 'split'
+vim.opt.incsearch = true
+vim.opt.list = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 5
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.title = true
+vim.opt.wrap = false
 
 if vim.fn.has("linux") then
   vim.opt.clipboard:append { 'unnamedplus' }
@@ -37,7 +37,7 @@ vim.cmd [[ autocmd FileType python setlocal ts=4 sts=4 sw=4 ]]
 -- C-indenting
 vim.cmd [[ autocmd FileType cpp setlocal ts=2 sts=2 sw=2 ]]
 vim.opt.cindent = true
-vim.opt.cino = 'g0, t0'
+vim.opt.cino = { 'g1, t0' }
 
 -- highlights
 vim.opt.termguicolors = true
