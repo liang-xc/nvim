be4cda287530fac3baf2e7eeef5ccf4748417f6a
LIANG X. <liangx0111@outlook.com>
2023-11-18 20:08:01 -0500

    ftplugin

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,6 +1,5 @@
-vim.opt.cindent = true
-vim.opt.cino = { "g1, t0" }
+vim.opt_local.cindent = true
+vim.opt_local.cino = { "g1, t0" }
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.tabstop = 2
-vim.opt.colorcolumn = "80"
