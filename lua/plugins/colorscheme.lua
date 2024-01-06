84fa03471a31957dc09d8a644d066d1230e1cb1e
LIANG Xiaocheng <97717068+liang-xc@users.noreply.github.com>
2024-01-06 16:56:11 -0500

    add clangd-extension fidget

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,14 @@ return {
   priority = 1000,
   config = function()
     require("nightfox").setup({
-  options = {
-    transparent = true,
-    styles = {
-      comments = "italic",
-    },
-  },
-})
+      options = {
+        transparent = true,
+        styles = {
+          comments = "italic",
+        },
+      },
+    })
 
-vim.cmd.colorscheme 'nordfox'
-end,
+    vim.cmd.colorscheme("nordfox")
+  end,
 }
