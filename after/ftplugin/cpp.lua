6416d7e52c39e023a0fbb028b8dac054c343fa43
LIANG Xiaocheng <97717068+liang-xc@users.noreply.github.com>
2024-01-22 23:44:41 -0500

    key bindings

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,2 +1,4 @@
 vim.opt_local.cindent = true
 vim.opt_local.cino = { "g1", "t0" }
+
+vim.keymap.set("n", "<leader>x", vim.cmd.ClangdSwitchSourceHeader, { desc = "Clangd switch source header" })
--- after/ftplugin/ocaml.lua
+++ after/ftplugin/ocaml.lua
@@ -1,1 +1,3 @@
 require("nvim-autopairs").remove_rule("'")
+require("nvim-autopairs").remove_rule("`")
+vim.opt_local.colorcolumn = "90"
