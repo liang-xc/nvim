e84035145805d12a084ea0fae52168f30b71915d
LIANG X. <liangx0111@outlook.com>
2024-02-16 00:19:18 -0500

    more lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,7 +15,18 @@ return {
 
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "cmake", "powershell_es", "rust_analyzer", "jsonls", "ruff_lsp" },
+        ensure_installed = {
+          "azure_pipelines_ls",
+          "cmake",
+          "jsonls",
+          "lemminx", -- xml
+          "lua_ls",
+          "pyright",
+          "powershell_es",
+          "ruff_lsp", -- python linter
+          "rust_analyzer",
+          "yamlls",
+        },
         handlers = {
           function(server_name) -- default handler
             require("lspconfig")[server_name].setup({
